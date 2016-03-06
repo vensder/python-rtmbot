fd8b365d32c77d8724a43b75382ebae038e55357
vensder <vensder@gmail.com>
2016-03-07 01:11:13 +0300

    inject some parameters from slack profile into data

--- plugins/canary/canary.py
+++ plugins/canary/canary.py
@@ -5,6 +5,6 @@ outputs = []
 def canary():
     #NOTE: you must add a real channel ID for this to work
     localtime = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-    outputs.append(["general", "bot started: " + str(localtime)])
+    outputs.append(["bots", "bot started: " + str(localtime)])
 
 canary()
--- plugins/repeat/repeat.py
+++ plugins/repeat/repeat.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
 #import time
 #crontable = [] # what for?
 outputs = []
 
 def process_message(data):
-    if data['channel'].startswith("C"):
-        print(data)
-        outputs.append([data['channel'], "from repeat1 \"{}\" in channel {}".format(data['text'], data['channel']) ])
-#        outputs.append(type(data))
-
+    #if data['channel'] == 'general':
+    #if data['channel'] == 'C0DEMSUG5':
+    print('type(data): ', type(data))
+    print('data: ', data)
+    outputs.append([data['channel'], "Hello from 'repeat' plugin \"{}\" in channel {} from user {}".format(data['text'], data['channel'], data['user']) ])
+    
+    if '@time' in data['text']:
+        print('time?')
+        outputs.append([data['channel'], " time?" + 'user: ' + data['real_name']])
+    
