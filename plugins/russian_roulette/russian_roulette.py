53c5608be7d4f6712104745684237f70f251e1f9
vensder <vensder@gmail.com>
2016-03-20 00:15:38 +0300

    russian roulette

--- plugins/russian_roulette/russian_roulette.py
+++ plugins/russian_roulette/russian_roulette.py
@@ -3,33 +3,28 @@
 
 #russian_roulette.py
 
-'''
+from slackclient import SlackClient
+from configparser import ConfigParser
+import os
+from random import choice
+
+parser = ConfigParser()
+parser.read(os.path.dirname(os.path.realpath(__file__)) + '/russian_roulette.conf')
+token = parser.get('slack', 'top-secret-user-token')
+
 def process_message(data):
     
     if 'text' in data:
         try:
             text = data['text']
             channel = data['channel']
-            
+
             if 'russian roulette' in text:
-                outputs.append([data['channel'], '<@' + data['user'] + '>--'])
-                outputs.append([data['channel'], '@pizza++'])
-                #outputs.append(["bots", "<@U0DEX55DL>, bot started at time: " + str(localtime)])
+                sc = SlackClient(token)
+                chan = data['channel']
+                shot = '<@' + data['user'] + '>' + choice(['++','--'])
+                print(sc.api_call("chat.postMessage", as_user="true:", channel=chan, text=shot))
+                del sc
 
         except KeyError:
             print('KeyError Exception')
-'''
-
-from slackclient import SlackClient
-from configparser import ConfigParser
-import os
-
-parser = ConfigParser()
-parser.read(os.path.dirname(os.path.realpath(__file__)) + '/russian_roulette.conf')
-
-token = parser.get('slack', 'top-secret-user-token')
-
-sc = SlackClient(token)
-chan = 'C0QN2S8J3'
-greeting = '<@U0QLT7CDS>: ++'
-print(sc.api_call("chat.postMessage", as_user="true:", channel=chan, text=greeting))
