4e7c7d15533351180f8d551185e2776437e4f9f6
vensder <vensder@gmail.com>
2016-05-06 12:52:44 +0300

    jenkins jobs help added

--- plugins/jenkins/jenkins_job0.py
+++ plugins/jenkins/jenkins_job0.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#from slackclient import SlackClient
 from configparser import ConfigParser
 from os import path
 import requests
@@ -9,6 +8,7 @@ import requests
 outputs = []
 jobs_dict = {} # dict of dicts with jobs configs
 slack_channels = set()
+help_text = ''
 
 Config = ConfigParser()
 Config.read(path.dirname(path.realpath(__file__)) + '/jenkins.cfg')
@@ -23,34 +23,28 @@ for section in Config.sections():
 print(jobs_dict.keys())
 print(jobs_dict.items())
 
+# collect slach channels into set
 for job in jobs_dict.keys():
     slack_channels.add(jobs_dict[job]['slack_channel'])
 print(slack_channels)
 
-# print help
+# collect help text
 for job in jobs_dict.keys():
-    print('{}: ```{}```'.format(jobs_dict[job]['help'], jobs_dict[job]['trigger']))
+    help_text = help_text + '{}: ```{}```\n'.format(jobs_dict[job]['help'], jobs_dict[job]['trigger'])
+print(help_text)
 
 def process_message(data):
-    print("jenkins_job0: " + str(data))
+    print("jenkins_job0.py: " + str(data))
     if 'text' in data:
         try:
             text = data['text']
-            chan = data['channel']
+            channel = data['channel']
             user = data['name']
-            print(SLACK_USERS)
-            
-            if TRIGGER_PHRASE in text and chan in slack_channels:
 
-                if not TRIGGER_PHRASE_TO_RUN in text:
-                    DRY_RUN = 'True'
-                    outputs.append([data['channel'], 'If you want to ' + TRIGGER_PHRASE_TO_RUN.lstrip('@') + ', try this: ```' + TRIGGER_PHRASE_TO_RUN + '```'])
-
-                elif TRIGGER_PHRASE_TO_RUN in text:
-                    DRY_RUN = 'False'
-
-                sc = SlackClient(SLACK_BOT_TOKEN)
-
+            if text.startwith('@jenkins help') and channel in slack_channels:
+                emoji = ':question:'
+                outputs.append([channel, help_text, 'Jenkins help', emoji])
+'''
                 if user in SLACK_USERS:
                     print(user)
                     print(text)
@@ -67,14 +61,13 @@ def process_message(data):
                     output_message = 'Jenkins job "' + JOB_NAME + '" started by ' + user + '\n'
                     output_message += 'DRY RUN: ' + DRY_RUN + '\n'
                     output_message += 'Status code: ' + str(status_code)
-                    print(sc.api_call("chat.postMessage", as_user="false", icon_emoji=":bowtie:", channel=chan, text=output_message))
+                    print(sc.api_call("chat.postMessage", as_user="false", icon_emoji=":bowtie:", channel=channel, text=output_message))
                     #outputs.append([data['channel'], output_message])
                 else:
                     output_message = 'Sorry, but user "' + user + '" doesn\'t have permission for this job :('
-                    print(sc.api_call("chat.postMessage", as_user="false", icon_emoji=":trollface:", channel=chan, text=output_message))
+                    print(sc.api_call("chat.postMessage", as_user="false", icon_emoji=":trollface:", channel=channel, text=output_message))
                     #outputs.append([data['channel'], 'Sorry, but user "' + user + '" doesn\'t have permission for this job :('])
-                del sc
-                
+'''
         except KeyError as e:
             print('KeyError Exception: ', e)
 
